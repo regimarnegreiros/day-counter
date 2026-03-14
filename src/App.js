c6bd12599ea6082b549b9c2d4c90a33348e1f89c
DanFonR <dannyelfr@yahoo.com.br>
2026-03-14 16:04:31 -0300

    Separa StyleSheet em arquivo .js próprio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Clock, Calendar, User, Plus } from 'lucide-react-native';
+import styles from './styles';
 
 export default function App() {
   return (
@@ -44,60 +45,3 @@ const TabItem = ({ icon, label, active }) => (
     <Text style={[styles.tabText, active && styles.tabActive]}>{label}</Text>
   </TouchableOpacity>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  
-  header: {
-    flexDirection: 'row',
-    padding: 10,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#eeeeee'
-  },
-  headerIconBox: {
-    backgroundColor: '#AD46FF',
-    padding: 10,
-    borderRadius: 12,
-    margin: 12,
-    alignSelf: 'flex-start'
-  },
-  headerTitle: {
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  headerSubtitle: {
-    fontSize: 12, color: '#777777'
-  },
-
-  tabBar: {
-    flexDirection: 'row',
-    height: 65,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  tabItem: {
-    alignItems: 'center'
-  },
-  tabLabel: {
-    fontSize: 10,
-    color: '#777777',
-    marginTop: 4
-  },
-  tabButton: {
-    flex: 1,
-    alignItems: 'center'
-  },
-
-  fab: {
-    position: 'absolute', right: 20, bottom: 90,
-    backgroundColor: '#AF52DE', width: 60, height: 60,
-    borderRadius: 30, justifyContent: 'center', alignItems: 'center',
-    elevation: 5, shadowColor: '#000', shadowOpacity: 0.3, shadowRadius: 5,
-  },
-});
