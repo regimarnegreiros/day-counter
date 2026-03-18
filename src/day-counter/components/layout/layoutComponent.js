9286c920a1756e26ad3bdeb5c42a0b2f29248dca
DanFonR <dannyelfr@yahoo.com.br>
2026-03-18 13:29:07 -0300

    Adiciona reatividade aos botões de MenuSelector; Tenta automatizar string de data em AppHeader

--- src/day-counter/components/layout/layoutComponent.js
+++ src/day-counter/components/layout/layoutComponent.js
@@ -1,31 +1,67 @@
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Clock, Calendar, User} from 'lucide-react-native';
 import styles from './layoutStyles';
+import { useState } from 'react';
 
-export const TabItem = ({ icon, label, active }) => (
-  <TouchableOpacity style={styles.tabButton}>
-    {icon}
+function makeIcon(Icon) {
+  return (active) => (<Icon style={active? styles.activeIcon : styles.inactiveIcon}/>);
+}
+
+export const TabItem = ({ icon, label, active, onPress }) => (
+  <TouchableOpacity style={styles.tabButton} onPress={onPress}>
+    {icon(active)}
     <Text style={[styles.tabText, active && styles.tabActive]}>{label}</Text>
   </TouchableOpacity>
 );
 
-export const MenuSelector = () => (
-  <View style={styles.tabBar}>
-    <TabItem icon={<Clock style={styles.activeIcon} />} label="Contadores" />
-    <TabItem icon={<Calendar style={styles.inactiveIcon} />} label="Ofensivas" active />
-    <TabItem icon={<User style={styles.inactiveIcon} />} label="Perfil" />
-  </View>
-);
+export const MenuSelector = () => {
+  const [activeTab, setActiveTab] = useState("Contadores");
 
+  return (
+    <View style={styles.tabBar}>
+      <TabItem
+        icon={makeIcon(Clock)}
+        label="Contadores"
+        active={activeTab === "Contadores"}
+        onPress={() => setActiveTab("Contadores")}
+      />
+      <TabItem
+        icon={makeIcon(Calendar)}
+        label="Ofensivas"
+        active={activeTab === "Ofensivas"}
+        onPress={() => setActiveTab("Ofensivas")}
+      />
+      <TabItem
+        icon={makeIcon(User)}
+        label="Perfil"
+        active={activeTab === "Perfil"}
+        onPress={() => setActiveTab("Perfil")}
+      />
+    </View>
+  )
+};
 
-export const AppHeader = ({title}) => (
-  <View style={styles.header}>
-    <View style={styles.headerIconBox}>
-      <Calendar color="white" size={20} />
+export const AppHeader = ({title}) => {
+  const data = new Date();
+  const mesString = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+  ][data.getMonth()];
+  const diaSemanaString = [
+    "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
+    "Quinta-feira", "Sexta-feira", "Sábado"
+  ][data.getDay()];
+  const dataString = `${diaSemanaString}, ${data.getDate()} de ${mesString} de ${data.getFullYear()}`;
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.headerIconBox}>
+        <Calendar color="white" size={20} />
+      </View>
+      <View>
+        <Text style={styles.headerTitle}>{title}</Text>
+        <Text style={styles.headerSubtitle}>{dataString}</Text>
+      </View>
     </View>
-    <View>
-      <Text style={styles.headerTitle}>{title}</Text>
-      <Text style={styles.headerSubtitle}>Quarta-feira, 11 de março de 2026</Text>
-    </View>
-  </View>
-);
+  )
+};
