6984ea713cfa4cdea9935efb085eb122124af26f
DanFonR <dannyelfr@yahoo.com.br>
2026-03-31 21:43:58 -0300

    Inclui SQLite3 e cria rota de checagem de status do banco de dados

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,47 @@
 //#region setup
 
 //#region imports
+
 import express, { Express, NextFunction, Request, Response } from "express";
-import { Configuration, error500Logger, exitStatus, HTTPCodes, loadConfig, printRequests } from "./utils.js";
+import sqlite3, { Database } from 'sqlite3';
+import {
+  Configuration, databaseHealthCheck, error500Logger,
+  exitStatus, HTTPCodes, loadConfig, requestLogger
+} from "./utils.js";
+
 //#endregion
 
 const serverData: Configuration = loadConfig("server-options.json");
-const [appIP, appPort] = [serverData.appIP, serverData.appPort];
-
 const app: Express = express();
+const db: Database = new Database(":memory:"); // Mudar depois?
 
 app.use(express.json());
-app.use(printRequests);
+app.use(requestLogger);
 
 //#endregion
 
 //#region GET
 
-app.get("/api/health", (req: Request, res: Response) => {
+app.get("/api/health", (res: Response) => {
+  if (process.uptime() < 1) // So services have time to start
+    return res.status(HTTPCodes.serviceUnavailable).send({ status: "starting" });
+
   res.status(HTTPCodes.ok).send({ status: "ok" });
+});
+
+app.get("/api/ready", async (req: Request, res: Response) => {
+  const dbOK: boolean = await databaseHealthCheck(db);
+
+  if (!dbOK) res.status(HTTPCodes.serviceUnavailable).send({
+    status: "degraded",
+    database: "down"
+  });
+
+  res.status(HTTPCodes.ok).send({
+    status: "ok",
+    database: "up"
+  });
+
 });
 
 app.get('/api/users/:id', (req: Request, res: Response) => {
@@ -33,11 +56,11 @@ app.get('/api/users/:id', (req: Request, res: Response) => {
 //#region POST
 
 app.post('/api/signup', (req: Request, res: Response) => {
-  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // to-implement
+  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // TODO
 });
 
 app.post('/api/signin', (req: Request, res: Response) => {
-  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // to-implement
+  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // TODO
 });
 
 //#endregion
@@ -45,7 +68,7 @@ app.post('/api/signin', (req: Request, res: Response) => {
 //#region PATCH
 
 app.patch('/api/syncevents', (req: Request, res: Response) => {
-  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // to-implement
+  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // TODO
 });
 
 //#endregion
@@ -53,7 +76,7 @@ app.patch('/api/syncevents', (req: Request, res: Response) => {
 //#region DELETE
 
 app.delete('/api/users/:id', (req: Request, res: Response) => {
-  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // to-implement
+  res.status(HTTPCodes.notImplemented).send({status: "not implemented"}); // TODO
 });
 
 //#endregion
@@ -66,8 +89,8 @@ app.use(error500Logger);
 
 //#region run
 
-const server = app.listen(appPort, appIP, () => {
-  console.log(`Serving @ http://${appIP}:${appPort}/`)
+const server = app.listen(serverData.appPort, serverData.appIP, () => {
+  console.log(`Serving @ http://${serverData.appIP}:${serverData.appPort}/`)
 });
 
 process.on("SIGINT", () => {
--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { PathOrFileDescriptor, readFileSync } from "fs";
 import { isIP } from "net";
+import { Database } from "sqlite3";
 
 //#region interfaces
 
@@ -14,7 +15,7 @@ export interface Configuration {
 
 //#region types
 
-type possibleConfig = Record<string, unknown>;
+
 
 //#endregion
 
@@ -31,30 +32,64 @@ class InvalidIPError extends Error {
 
 //#region functions
 
-function isConfiguration(obj: unknown): obj is Configuration {
-  if (obj === null || typeof(obj) !== "object") return false;
+/**
+ * Performs a database connection check with a SELECT statement
+ * @param {Database} db An open database connection
+ * @returns {Promise<void>}
+ * A boolean. True if the statement went through, false if not
+ */
+export async function databaseHealthCheck(db: Database): Promise<boolean> {
+  try {
+    await new Promise<void>((resolve, reject) => {
+      db.get("SELECT 1", (err: Error | null) => {
+        if (err) reject(err);
+        else resolve();
+      })
+    });
 
-  const castObj = obj as possibleConfig;
-    return (typeof(castObj.appIP) === "string"
-      && typeof(castObj.appPort) === "number"
-    );
+    return true;
+  }
+  catch {
+    return false;
+  }
 }
 
+function isConfiguration(obj: unknown): obj is Configuration {
+  if (obj === null || typeof(obj) !== "object" || Array.isArray(obj)) return false;
+
+  return (
+    "appIP" in obj && "appPort" in obj
+    && typeof(obj.appIP) === "string"
+    && typeof(obj.appPort) === "number"
+  );
+}
+
+/**
+ * @description
+ * Reads a JSON file specifying IP and port to serve the Express app.
+ * If an error occurs during parsing, or if the given configuration is invalid,
+ * logs the error and returns '127.0.0.1' as the IP and 3000 as the port.
+ * @param {PathOrFileDescriptor} path
+ * JSON configuration file path
+ * @returns {Configuration}
+ * A configuration, either the specified one
+ * or a default, if an error occurs during parsing or validation
+ */
 export function loadConfig(path: PathOrFileDescriptor): Configuration {
   try {
     const config: unknown = JSON.parse(readFileSync(path, "utf-8"));
 
     if (!isConfiguration(config))
-      throw new TypeError("Invalid type");
+      throw new TypeError("Invalid configuration fields");
     if (isIP(config.appIP) === 0)
       throw new InvalidIPError("Invalid IP");
-    if (config.appPort < 0 || config.appPort > 65535)
+    if (!Number.isInteger(config.appPort) || config.appPort < 0 || config.appPort > 65535)
       throw new RangeError("Invalid port range");
 
     return {appIP: config.appIP, appPort: config.appPort};
   }
   catch (err) {
-    if (err instanceof SyntaxError)
+    if (err instanceof SyntaxError) // Parsing error
       console.error("Invalid JSON format");
     else
       console.error((err as Error).message);
@@ -65,11 +100,11 @@ export function loadConfig(path: PathOrFileDescriptor): Configuration {
   }
 }
 
-export function printRequests(req: Request, res: Response, next: NextFunction): void {
-  console.debug(`method: ${req.method}`);
-  console.debug(`url: ${req.url}`);
-  console.debug(`header: ${JSON.stringify(req.headers, null, 2)}`);
-  console.debug(`body: ${JSON.stringify(req.body, null, 2)}`);
+export function requestLogger(req: Request, res: Response, next: NextFunction): void {
+  console.debug(`${req.method} REQUEST @ ${req.url}`);
+  console.debug(`COOKIES: ${JSON.stringify(req.cookies, null, 2)}\n`);
+  console.debug(`HEADER: ${JSON.stringify(req.headers, null, 2)}\n`);
+  console.debug(`BODY: ${JSON.stringify(req.body, null, 2)}\n`);
   next();
 }
 
@@ -89,16 +124,28 @@ export const exitStatus = Object.freeze({
 });
 
 export const HTTPCodes = Object.freeze({
+  /** Successful request */
   ok: 200,
+  /** New resoruce created */
   created: 201,
+  /** Accessible via alternate URI in Location header */
   found: 302,
+  /** Client error, server rejects request */
   badRequest: 400,
+  /** Authentication required */
   unauthorized: 401,
+  /** Not enough permissions */
   forbidden: 403,
+  /** Resource not found */
   notFound: 404,
+  /** Request took too long, server rejected */
   requestTimeout: 408,
+  /** Generic server error */
   internalError: 500,
+  /** Request method not recognized or can't fulfil request */
   notImplemented: 501,
+  /** Server not ready to fulfil request (overload, down for maintenance, etc.) */
+  serviceUnavailable: 503,
 
 });
 
