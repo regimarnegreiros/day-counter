1d80e96100976235cc0c8c7639a90f187f8d00e4
DanFonR <dannyelfr@yahoo.com.br>
2026-04-26 02:28:01 -0300

    feat: Cria schema do banco de dados Possui 2 tabelas e uma trigger. Schema criado por Davi Roberto Barros Ferreira, correções e trigger por Dannyel Fontenele Ribeiro.

---
+++ src/api/schema.sql
@@ -0,0 +1,41 @@
+PRAGMA foreign_keys = true;
+
+CREATE TABLE IF NOT EXISTS user(
+	id CHAR(36) PRIMARY KEY, -- UUID
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL, -- HASH + SALT !!!
+);
+CREATE TABLE IF NOT EXISTS counters(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	icon VARCHAR(2) NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	type CHAR(1) NOT NULL CHECK(type IN ('r','p')), -- [R]egressive, [P]rogressive
+	start_date CHAR(10) NOT NULL,
+	end_date CHAR(10),
+	description TEXT,
+	hue INT NOT NULL CHECK(hue >= 0 AND hue <= 360),
+	notify_interval CHAR(1) NOT NULL,
+	user_id CHAR(36) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+
+	CONSTRAINT verify_end_date_needed CHECK(
+		(type = 'r' AND date(end_date) IS NOT NULL) OR
+		(type = 'p' AND end_date IS NULL)
+	),
+	CONSTRAINT verify_start_date_format CHECK(
+		date(start_date) IS NOT NULL
+	)
+);
+
+CREATE TRIGGER IF NOT EXISTS date_fmt AFTER INSERT ON counters
+    BEGIN
+        UPDATE counters
+        SET
+            start_date = date(start_date);
+            end_date = CASE
+                WHEN end_date IS NOT NULL THEN date(end_date)
+                ELSE NULL
+            END
+        WHERE id = NEW.id;
+    END;
